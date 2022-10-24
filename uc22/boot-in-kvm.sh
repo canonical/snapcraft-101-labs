13d23dd30cd8cc9fb4a974c99f03e15296f22c3e
Robert P. J. Day <robert.day@canonical.com>
2022-10-24 09:28:14 -0400

    boot-in-kvm.sh: add QEMU invocation script
    Signed-off-by: Robert P. J. Day <robert.day@canonical.com>

---
+++ uc22/boot-in-kvm.sh
@@ -0,0 +1,14 @@
+sudo qemu-system-x86_64 \
+ -enable-kvm \
+ -smp 1 \
+ -m 2048 \
+ -machine q35 \
+ -cpu host \
+ -global ICH9-LPC.disable_s3=1 \
+ -net nic,model=virtio \
+ -net user,hostfwd=tcp::8022-:22,hostfwd=tcp::8090-:80  \
+ -drive file=/usr/share/OVMF/OVMF_CODE.secboot.fd,if=pflash,format=raw,unit=0,readonly=on \
+ -drive file=/usr/share/OVMF/OVMF_VARS.ms.fd,if=pflash,format=raw,unit=1 \
+ -drive "file=${1}",if=none,format=raw,id=disk1 \
+ -device virtio-blk-pci,drive=disk1,bootindex=1 \
+ -serial mon:stdio
